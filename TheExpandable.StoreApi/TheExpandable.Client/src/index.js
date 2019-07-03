92429824910f411a1459dd4eafbcdefeac3b5eb7
Unknown <behnam.iam@gmail.com>
2019-07-04 00:44:14 +0100

    refactored the initial react

--- TheExpandable.StoreApi/TheExpandable.Client/src/index.js
+++ TheExpandable.StoreApi/TheExpandable.Client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import App from './components/App';
-
-ReactDOM.hydrate(
-  <App />,
-  document.getElementById('mountNode'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
