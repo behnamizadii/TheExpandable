bd889d6cffe1cedf7f15a1b1df3820f4b765f70c
Unknown <behnam.iam@gmail.com>
2019-07-05 12:04:37 +0100

    working navigation

--- TheExpandable.StoreApi/TheExpandable.Client/src/App.js
+++ TheExpandable.StoreApi/TheExpandable.Client/src/App.js
@@ -1,43 +1,120 @@
-import React, { Component} from 'react';
-import MyComponent from './MyComponent';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Table } from 'reactstrap';
+import NavBar from './Navbar';
+import axios from 'axios';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      title: 'app title'
-    }
-
-    this.onClick = this.onClick.bind(this);
+  state = {
+    storeItems: []
   }
 
-  onClick() {
-    this.setState({
-      title: 'OOooo Clicked!'
+  componentWillMount(){
+    axios.get('https://localhost:5001/Home/GetAll').then((response) =>{
+      this.setState({
+        storeItems: response.data
+      })
     });
   }
-
-  render() {
-    const title = "Welcome to our Web Store :)";
-
+  render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>{this.state.title}</h1>
-        </header>
-        <body>
-          <MyComponent 
-          title="This is component title"
-          name="This is component name"
-          onClick={this.onClick}
-          />
-        </body>
-      </div>
+      <Router>
+        <NavBar />  
+        <Header />
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+          <Route path="/items" component={Items} />
+        </div>
+      </Router>
     );
   }
+}
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topic({ match }) {
+  return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Item({ match }) {
+  return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Items({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/allitems`}>All Items</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={Item} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <Table>
+      <thead>
+        <th>
+          <Link to="/">Home</Link>
+        </th>
+        <th>
+          <Link to="/about">About</Link>
+        </th>
+        <th>
+          <Link to="/topics">Topics</Link>
+        </th>
+        <th>
+          <Link to="/items">Items</Link>
+        </th>
+      </thead>
+    </Table>
+  );
 }
 
 export default App;
